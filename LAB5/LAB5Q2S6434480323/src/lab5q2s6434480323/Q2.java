c128e22ea29e0187e2baba29348c7d776e9ce99d
Ratchanon Panmas <qq.panmas@gmail.com>
2022-02-13 09:50:30 +0700

    :heavy_plus_sign: Edit output and fix print case

--- LAB5/LAB5Q2S6434480323/src/lab5q2s6434480323/Q2.java
+++ LAB5/LAB5Q2S6434480323/src/lab5q2s6434480323/Q2.java
@@ -27,7 +27,7 @@ public class Q2 {
         int credit;
         
         // Course 1
-        System.out.println("Enter course ID, course name, credit:");
+        System.out.print("Enter course ID, course name, credit: ");
         input = scanner.nextLine().split(" ");
         
         courseId = Integer.parseInt(input[0]);
@@ -37,7 +37,7 @@ public class Q2 {
         Course course1 = new Course(courseName, courseId, credit);
         
         // Course 2
-        System.out.println("Enter course ID, course name:");
+        System.out.print("Enter course ID, course name: ");
         input = scanner.nextLine().split(" ");
         courseId = Integer.parseInt(input[0]);
         courseName = String.join(" ", Arrays.copyOfRange(input, 1, input.length));
@@ -47,7 +47,7 @@ public class Q2 {
         // Check is same course
        if (course1.equals(course2)){
            System.out.println("Same course");
-           System.out.println("Enter course ID, course name:");
+           System.out.print("Enter course ID, course name: ");
            
            input = scanner.nextLine().split(" ");
         
@@ -59,11 +59,30 @@ public class Q2 {
            course2.setCname(courseName);
            course2.setCredit(credit);
        } else {
-           System.out.println("Different course name, course ID and credit.");
+           System.out.println(compareToString(course1.compare(course2)));
        }
        
         System.out.println(course1.toString());
         System.out.println(course2.toString());
-        System.out.println("Total credit: " + (course1.getCredit() + course2.getCredit()));
-    } 
+        System.out.println("Total credit: " + course1.sumCredit(course2));
+    }
+    
+    /**
+     *
+     * @param type
+     * @return String that show what is different
+     */
+    public static String compareToString(int type){
+        return switch (type) {
+            case 0 -> "No different";
+            case 1 -> "Different course ID";
+            case 2 -> "Different course name";
+            case 3 -> "Different credit";
+            case 4 -> "Different course name and credit";
+            case 5 -> "Different course ID and credit";
+            case 6 -> "Different course ID and course name";
+            case 7 -> "Different course ID, course Name and credit";
+            default -> "Error Not match any case";
+        };
+    }
 }
